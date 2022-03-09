cc36c91941ebdd2e3555a011d23a5da79d8cdf9e
bjday <brahim.jday@yahoo.fr>
2022-03-09 10:01:05 +0100

    contact template done

--- html/contact.php
+++ html/contact.php
@@ -1,31 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php  require './connect.php'; 
+require_once './header.php'; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenue !</title>
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-</head>
 
-<body>
-    <header>
-        <div class="title-page"> <i class="far fa-calendar-check reminder"></i>
-        <span> <a href="../index.html"> Mon pense bête</a></span> </div>
-        <ul>
-            
-            <li> <a href="/html/liste-courses.html">Liste Courses</a></li>
-            <li> <a href="/html/rdv.html">Rdv importants</a></li>
-            <li> <a href="/html/ce_week-end.html">On fait quoi Ce week-end</a></li>
-            <li> <a href="/html/ne_pas_oublier.html">A ne pas Oublier</a></li>
-            <li> <a href="/html/todolist.html">To do list</a></li>
-            <li> <a href="/html/repas_du_soir.html">On mange quoi ce soir</a></li>
-            <li> <a href="/html/contact.html">Contact</a></li>
-        </ul>
-    </header>
     <main>
         <h1>Pour me contacter</h1>
         <p><!-- trait horizontal noir --></p>
@@ -51,6 +27,7 @@
                     <li><input type="reset" value="Reset" /></li>
                 </ul>
             </form>
+            
         </div>
     </main>
 </body>
