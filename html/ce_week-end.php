5681842bbc9f7b723e9acc701e74fb92d1ffd6ba
Brahim Jday <jdayinvestis@gmail.com>
2022-04-26 14:49:13 +0200

    switched from linux to windows

--- html/ce_week-end.php
+++ html/ce_week-end.php
@@ -1,128 +1,128 @@
 <?php require './connect.php';
 require_once './header.php'; ?>
 
-    <main>
-        <h1>Programme du week-end </h1>
-        <p><!-- trait horizontal noir --></p>
-    </main>
+<main>
+<h1>Programme du week-end </h1>
+<p><!-- trait horizontal noir --></p>
+</main>
 
 
 
 
 <div class="container">
-            <div class="jumbotron">
-            <hr class="my-4">
-            </div>
-          
-              <!-- Button trigger modal -->
-              <button type="button" class="badge btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Ajouter<span class="badge bg-secondary"></span>
-              </button>
-              <!-- Modal -->
-              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
+<div class="jumbotron">
+<hr class="my-4">
+</div>
 
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Ajouter une Liste</h5>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
+<!-- Button trigger modal -->
+<button type="button" class="badge btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+Ajouter<span class="badge bg-secondary"></span>
+</button>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content">
 
-                    <div class="modal-body">
+<div class="modal-header">
+<h5 class="modal-title" id="exampleModalLabel">Ajouter une Liste</h5>
+<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
 
-                      <form  method="POST">
-                      <div class="mb-3">
-                        <label  class="form-label" >Nom</label>
-                        <input type="text" class="form-control" name="nom"  required>
-                      </div>
-                      <div class="mb-3">
-                        <label  class="form-label">Date</label>
-                        <input type="text" class="form-control"  name="date" required>
-                      </div>
-                      <div class="mb-3">
-                        <label  class="form-label" >Heure</label>
-                        <input type="text" class="form-control"  name="heure" required>
-                      </div> 
-                      <div class="mb-3">
-                        <label  class="form-label" >Commentaire</label>
-                        <input type="text" class="form-control"  name="commentaire" required>
-                      </div> 
+<div class="modal-body">
 
-                      <button  class="btn btn-primary" name="soumettre" >Ajouter</button>
-                    </form>
+<form  method="POST">
+<div class="mb-3">
+<label  class="form-label" >Nom</label>
+<input type="text" class="form-control" name="nom"  required>
+</div>
+<div class="mb-3">
+<label  class="form-label">Date</label>
+<input type="text" class="form-control"  name="date" required>
+</div>
+<div class="mb-3">
+<label  class="form-label" >Heure</label>
+<input type="text" class="form-control"  name="heure" required>
+</div>
+<div class="mb-3">
+<label  class="form-label" >Commentaire</label>
+<input type="text" class="form-control"  name="commentaire" required>
+</div>
 
-
-                    </div>
-  
-                  </div>
-                </div>
-              </div>
+<button  class="btn btn-primary" name="soumettre" >Ajouter</button>
+</form>
 
 
-            <hr class="my-4">
+</div>
 
-              </div>
+</div>
+</div>
+</div>
+
+
+<hr class="my-4">
+
+</div>
 
 
 <div class="container">
 <table class="table table-hover">
-  <thead class="table-dark">
-    <tr>
-     
-      <th scope="col">Nom</th>
-      <th scope="col">Date</th>
-      <th scope="col">Heure</th>
-      <th scope="col">Commentaire</th>
-      <th scope="col">Détails</th>
-      <th scope="col">Supprimer</th>
-      
-    </tr>
-  </thead>
-  <tbody >
-        <tr>
+<thead class="table-dark">
+<tr>
+
+<th scope="col">Nom</th>
+<th scope="col">Date</th>
+<th scope="col">Heure</th>
+<th scope="col">Commentaire</th>
+<th scope="col">Détails</th>
+<th scope="col">Supprimer</th>
+
+</tr>
+</thead>
+<tbody >
+<tr>
 <?php foreach($selectElement as $listWe){?>
-<td ><?php echo $listWe['nom'];?></td>
-<td ><?php echo $listWe['date'];?></td>
-<td ><?php echo $listWe['heure'];?></td>
-<td ><?php echo $listWe['commentaire'];?></td>
-<td><button type="button" class="detail badge btn-primary" data-bs-toggle="modal" data-bs-target="#detailModal">Détail</button></td> 
+  <td ><?php echo $listWe['nom'];?></td>
+  <td ><?php echo $listWe['date'];?></td>
+  <td ><?php echo $listWe['heure'];?></td>
+  <td ><?php echo $listWe['commentaire'];?></td>
+  <td><button type="button" class="detail badge btn-primary" data-bs-toggle="modal" data-bs-target="#detailModal">Détail</button></td>
 
-<td><?php echo "<form method='POST'> <button class='badge  btn-danger' name ='remove_weekend' value='" . $listWe['id']. "' type='submit'>Supprimer</button>" ;?></td>
+  <td><?php echo "<form method='POST'> <button class='badge  btn-danger' name ='remove_weekend' value='" . $listWe['id']. "' type='submit'>Supprimer</button>" ;?></td>
 
-        </tr>
-       <?php }?>
+  </tr>
+  <?php }?>
 
-     </tbody>
-</table>
-
-
-</div>
+  </tbody>
+  </table>
 
 
-<!-- Modal -->
-<div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-
-           <?php echo $listWe['nom']; ?> 
-       
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
-      </div>
-    </div>
   </div>
-</div>
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+  <div class="modal-content">
+  <div class="modal-header">
+  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  </div>
+  <div class="modal-body">
+
+  <?php echo $listWe['nom']; ?>
+
+  </div>
+  <div class="modal-footer">
+  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+  <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+  </div>
+  </div>
+  </div>
+  </div>
 
 
 
-<?php require_once './footer.php'; ?>
+  <?php require_once './footer.php'; ?>
 
 
 
